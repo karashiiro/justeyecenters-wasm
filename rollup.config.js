a077a9602b7f0bc661da04c5ba0aaa2e9d32bcb5
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-04-30 23:46:08 -0700

    Don't load sync wasm

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,15 @@
 import typescript from "@rollup/plugin-typescript";
 import { wasm } from "@rollup/plugin-wasm";
 
+const KILOBYTE = 1024;
+
 export default {
     input: "src/index.ts",
     output: {
         dir: "lib",
         format: "cjs",
     },
-    plugins:[typescript(), wasm({ sync: ["src/lib.wasm"] })]
+    // 999KB should never realistically be exceeded, but I'm including
+    // this comment so that this line is brought to mind if it ever is.
+    plugins:[typescript(), wasm({ maxFileSize: 999 * KILOBYTE })]
 };
